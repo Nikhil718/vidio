a6f02fc5b640a66c3a6dcb8f317d959a77613f5a
Nikhil <Naiknikhil925@gmail.com>
2023-03-09 00:12:37 +0530

    updated constants.

--- src/Components/Config/Constant.js
+++ src/Components/Config/Constant.js
@@ -1,4 +1,6 @@
-const API_KEY = "AIzaSyAeIG1Bvmz6nVlcpboYG4ZYxcLHGPuNeTM";
+import { config } from "./config";
+var API_KEY = config.API_KEY;
+
 export const YOUTUBE_VIDEO_URL =
   " https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   API_KEY;
---
+++ src/Components/Config/config.js
@@ -0,0 +1,3 @@
+export const config = {
+  API_KEY: "AIzaSyAeIG1Bvmz6nVlcpboYG4ZYxcLHGPuNeTM",
+};
